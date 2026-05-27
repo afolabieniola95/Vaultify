7b51e3bf49d28256b98aecc03f5ec00760081cf3
afolabieniola95 <afolabieniola95@gmail.com>
2026-05-27 17:59:49 +0100

    create home.js

---
+++ home.js
@@ -0,0 +1,59 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+  const currentUser = window.getCurrentUser();
+
+  if (!currentUser) {
+    window.location.href = "index.html";
+    return;
+  }
+
+  // DOM
+  const nameEl = document.getElementById("user-name");
+  const balanceEl = document.getElementById("user-balance");
+  const avatarDiv = document.getElementById("user-avatar");
+  const symbol = document.getElementById("symbol");
+
+  // User data
+  nameEl.textContent = currentUser.name;
+  balanceEl.textContent = currentUser.balance;
+  symbol.innerHTML = "&#8358";
+
+  avatarDiv.style.backgroundImage = currentUser.avatar
+    ? `url(${currentUser.avatar})`
+    : `url(icon/no-profile.png)`;
+
+  // Toggle balance
+  let toggled = false;
+
+  const toggle = document.querySelectorAll('.toggle-balance');
+  const opened = document.getElementById('opened');
+  const closed = document.getElementById('closed');
+
+  toggle.forEach(btn => {
+
+    btn.addEventListener('click', () => {
+
+      if (!toggled) {
+
+        opened.style.display = "none";
+        closed.style.display = "block";
+
+        balanceEl.textContent = "****";
+        symbol.innerHTML = "";
+        toggled = true;
+
+      } else {
+
+        opened.style.display = "block";
+        closed.style.display = "none";
+
+        balanceEl.textContent = currentUser.balance;
+        symbol.innerHTML = "&#8358";
+        toggled = false;
+      }
+
+    });
+
+  });
+
+});
